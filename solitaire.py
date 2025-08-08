55792120a717ea26514c94d9f9d1bf7196d77af0
marosimarcell13 <63652125+marosimarcell13@users.noreply.github.com>
2025-08-08 13:01:24 +0200

    Bugfixes, feature requests added
    Fixed issues when user input was corrupted
    Added table print feature
    Added print feature for locked cards (not listed in commands)
    Refactored some parts of the codebase

--- solitaire.py
+++ solitaire.py
@@ -1,5 +1,5 @@
 import random
-BREAK_STRING = "---------------------------------------------------------------------------------------------------------"
+BREAK_STRING = "----------------------------------------------------------------------"
 
 class Card():
     card_to_name = {7:"7", 8:"8", 9:"9", 10:"10", 11:"A", 12:"F", 13:"K", 14:"ZS"}
@@ -28,7 +28,7 @@ class Card():
             return False
     
     def __str__(self):
-        return f"{self.name} of {self.suit}"
+        return f"{self.name} {self.suit}"
 
     def __repr__(self):
         return self.__str__()
@@ -52,11 +52,12 @@ class Deck():
     def __repr__(self):
         return self.__str__()
 
-class DeckWaste():
+class DeckWasteLock():
 
     def __init__(self, cards):
         self.cards = cards
         self.waste = []
+        self.locked = self.cards.deal(4)
 
     def deal(self):
         if len(self.cards.deck) > 0:
@@ -76,6 +77,17 @@ class DeckWaste():
         else:
             return "empty    "
 
+    def autoDraw(self):
+        if len(self.waste) == 0:
+            self.deal()
+
+    def moveLockedToWaste(self):
+        """Move one locked card to waste if conditions match."""
+        if not self.waste and self.locked and not self.cards.deck:
+            self.waste.append(self.locked.pop())
+        else:
+            print("Waste is not empty or there is no more locked card")
+            
     def __str__(self):
         return str(self.waste)
 
@@ -93,37 +105,47 @@ class Table():
     def stackAttr(self, attrNum):
         if int(attrNum) > 4 or int(attrNum) < 1:
             print("**THERE IS ONLY 4 STACKS IN THE TABLE**")
-            return
+            return False
         return getattr(self, f"stack_{attrNum}")
 
-    def moveInTableMore(self, stackNumberFrom, stackNumberTo, topCard):
+    def moveInTableUniversal(self, stackNumberFrom, stackNumberTo, topCard=None):
         stackFrom = self.stackAttr(stackNumberFrom)
         stackTo = self.stackAttr(stackNumberTo)
-        cnt = None
-        if stackFrom:
-            for i in range(0,len(stackFrom)):
-                if stackFrom[i].name.lower() == topCard:
-                    cnt = i
-                    break
-            if cnt is None:
-                print(f"No {topCard} is in the stack")
-                return
-            print(cnt)
-            if stackTo:
-                if stackTo[-1].isDiffSuit(stackFrom[cnt]) and stackTo[-1].isGreater(stackFrom[cnt]):
-                    stackTo.extend(stackFrom[cnt:])
-                else:
-                    print("Card is invalid to move")
-                    return
-            elif stackTo is not None:
-                stackTo.extend(stackFrom[cnt:])
-                return
-            else:
-                print("No stack was created")
-                return
-            del stackFrom[cnt:]
+    
+        if not stackFrom:
+            print(f"No card in stack_{stackNumberFrom}")
+            return
+    
+        # Determine the starting index of cards to move
+        if topCard is None:
+            # Move only the last card
+            start_index = len(stackFrom) - 1
         else:
-            print("Stack is empty")
+            # Find the card in the stack
+            start_index = None
+            for i, card in enumerate(stackFrom):
+                if card.name.lower() == topCard.lower():
+                    start_index = i
+                    break
+            if start_index is None:
+                print(f"No {topCard} in stack_{stackNumberFrom}")
+                return
+    
+        # Cards to move
+        moving_cards = stackFrom[start_index:]
+    
+        # Moving logic
+        if stackTo:
+            if stackTo[-1].isDiffSuit(moving_cards[0]) and stackTo[-1].isGreater(moving_cards[0]):
+                stackTo.extend(moving_cards)
+            else:
+                print("Card is invalid to move")
+                return
+        else:
+            stackTo.extend(moving_cards)
+    
+        # Remove moved cards from source
+        del stackFrom[start_index:]
 
     def maxLength(self):
         return max(len(self.stack_1), len(self.stack_2), len(self.stack_3), len(self.stack_4))
@@ -133,7 +155,7 @@ class Table():
         if len(stack) > elementNum:
             return stack[elementNum]
         else:
-            return "         "
+            return "    "
 
     def addFromWaste(self, w, stackNumber):
         if int(stackNumber) < 1 or int(stackNumber) > 4:
@@ -151,20 +173,6 @@ class Table():
                     print("Card to add is invalid")
             else:
                 print("Invalid stack")
-
-    def moveInTable(self, stackNumberFrom, stackNumberTo):
-        stackFrom = self.stackAttr(stackNumberFrom)
-        stackTo = self.stackAttr(stackNumberTo)
-        if stackFrom:
-            if stackTo:
-                if stackFrom[-1].isBelow(stackTo[-1]) and stackFrom[-1].isDiffSuit(stackTo[-1]):
-                    stackTo.append(stackFrom.pop())
-                else:
-                    print("Card is invalid to move")
-            else:
-                stackTo.append(stackFrom.pop())
-        else:
-            print(f"No card in stack_{stackNumberFrom}")
         
 
 
@@ -179,7 +187,7 @@ class Foundation():
     def foundationAttr(self, attrKind):
         if attrKind == "tok" or attrKind == "sziv" or attrKind == "zold" or attrKind == "makk":
             return  getattr(self, f"foundation_{attrKind}")
-        print("**FOUNDATION CAN BE ONLY A,F,K,ZS**")
+        print("**FOUNDATION CAN BE ONLY T,SZ,Z,M**")
     
     def isWon(self):
         if len(self.foundation_tok) > 0 and len(self.foundation_sziv) > 0 and len(self.foundation_zold) > 0 and len(self.foundation_makk) > 0:
@@ -189,91 +197,95 @@ class Foundation():
     def getTopCard(self, suit):
         foundationGet = self.foundationAttr(suit)
         if len(foundationGet) == 0:
-            return suit[0].upper()
+            return f"{suit[0].upper()}    "
         else:
             return foundationGet[-1]
 
     def addFromWaste(self, w):
-        if len(w.waste) > 0: 
-            cardToAdd = w.waste[-1]
-            foundationToAdd = self.foundationAttr(cardToAdd.suit)
-            if len(foundationToAdd) == 0 and cardToAdd.value == 7:
-                foundationToAdd.append(w.waste.pop())
-            elif len(foundationToAdd) > 0:
-                if cardToAdd.isGreater(foundationToAdd[-1]):
-                    foundationToAdd.append(w.waste.pop())
-                else:
-                    print("Invalid card to add to the foundation")
-            else:
-                print("Foundation can only start with 7")
-        else:
-            print("No card in waste")
+        self._addCardToFoundation(w.waste)
 
-    def addFromTableStack(self, stack):
-        if len(stack) > 0:
-            cardToAdd = stack[-1]
-            foundationToAdd = self.foundationAttr(cardToAdd.suit)
-            if len(foundationToAdd) == 0 and cardToAdd.value == 7:
-                foundationToAdd.append(stack.pop())
-            elif len(foundationToAdd) > 0:
-                if cardToAdd.isGreater(foundationToAdd[-1]):
-                    foundationToAdd.append(stack.pop())
-                else:
-                    print("Invalid card to add to the foundation")
+    def addFromTableStack(self, stack, stackNum):
+        stackFrom = stack.stackAttr(stackNum)
+        self._addCardToFoundation(stackFrom)
+
+    def _addCardToFoundation(self, source_pile):
+        """Internal reusable logic to move top card from source pile to foundation."""
+        if len(source_pile) == 0:
+            print("No card to move")
+            return
+
+        cardToAdd = source_pile[-1]
+        foundationToAdd = self.foundationAttr(cardToAdd.suit)
+
+        if len(foundationToAdd) == 0 and cardToAdd.value == 7:
+            foundationToAdd.append(source_pile.pop())
+        elif len(foundationToAdd) > 0:
+            if cardToAdd.isGreater(foundationToAdd[-1]):
+                foundationToAdd.append(source_pile.pop())
             else:
-                print("Foundation can only start with 7")
+                print("Invalid card to add to the foundation")
         else:
-            print("No card in stack")
+            print("Foundation can only start with 7")
     
 def printCommands():
     print("Valid commands:")
-    print("\tdw - move card from deck to waste")
-    print("\twf - move card from waste to foundation")
-    print("\twt #T- move card from waste to table")
-    print("\ttf #T- move card from table to foundation")
-    print("\ttt #T1 #T2 - move card between stacks")
-    print("\ttm #T1 #T2 #CN - move carcs from one stock to an other")
-    print("\tlw - move card from locked to waste")
-    print("\th - help")
-    print("\tq - quit")
+    print("\th              - help")
+    print("\tq              - quit")
+    print("\tpt             - prints the gaming area")
+    print("\tta             - toggle auto draw (default: OFF)")
+    print("\tdw             - move card from deck to waste")
+    print("\twf             - move card from waste to foundation")
+    print("\twt #T          - move card from waste to table")
+    print("\ttf #T          - move card from table to foundation")
+    print("\ttt #T1 #T2     - move cards between stacks")
+    print("\ttt #T1 #T2 #CV - move cards between stacks (CV = Card Value)")
+    print("\tlw             - move card from locked to waste")
 
-def printTable(table, foundation, waste, locked):
+def printTable(table, foundation, waste):
     print(BREAK_STRING)
-    print("Locked \t  Waste \t Deck \t\t\t Foundation")
-    print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
-        len(locked),
-        waste.getWaste(),
-        waste.getDeck(),
-        foundation.getTopCard("tok"),
-        foundation.getTopCard("sziv"),
-        foundation.getTopCard("zold"),
-        foundation.getTopCard("makk")
-    ))
-    print("\nTable\n\t1\t\t2\t\t3\t\t4")
-    for x in range(0,table.maxLength()):
-        print("\t{}\t{}\t{}\t{}".format(
-            table.getElement(1,x),
-            table.getElement(2,x),
-            table.getElement(3,x),
-            table.getElement(4,x)
-        ))
-    print("\n"+BREAK_STRING)
+    print("Lock    Waste      Deck                   Foundation")
+    print(
+        "{:<7} {:10} {:<9}    {:<8} {:<8} {:<8} {:<8}".format(
+            len(waste.locked),
+            str(waste.getWaste()),
+            waste.getDeck(),
+            str(foundation.getTopCard("tok")),
+            str(foundation.getTopCard("sziv")),
+            str(foundation.getTopCard("zold")),
+            str(foundation.getTopCard("makk"))
+        )
+    )
+
+    print("\nTable")
+    print("        {:<10} {:<10} {:<10} {:<10}".format("1", "2", "3", "4"))
+    for x in range(table.maxLength()):
+        print(
+            "      {:<10} {:<10} {:<10} {:<10}".format(
+                str(table.getElement(1, x)),
+                str(table.getElement(2, x)),
+                str(table.getElement(3, x)),
+                str(table.getElement(4, x))
+            )
+        )
+    print("\n" + BREAK_STRING)
 
 
 if __name__ == "__main__":
     d = Deck()
     f = Foundation()
-    locked = d.deal(4)
-    w = DeckWaste(d)
+    w = DeckWasteLock(d)
     t = Table()
     printCommands()
+    autoDraw = False
     #Fill the board
     for n in range(1,5):
         w.deal()
         t.addFromWaste(w.waste,n)
-    printTable(t, f, w, locked)
-    q = True
     while not f.isWon():
+        if autoDraw:
+            w.autoDraw()
+        printTable(t, f, w)
+
         command = input("Enter a command (type 'h' for help): ")
         command = command.lower().replace(" ", "")
 
@@ -284,33 +296,31 @@ if __name__ == "__main__":
             break
         elif command == "dw":
             w.deal()
-            printTable(t, f, w, locked)
+        elif command == "pt":
+            continue
+        elif command == "ta":
+            print("Auto draw toggeled")
+            autoDraw = not autoDraw
         elif command == "wf":
             f.addFromWaste(w)
-            printTable(t, f, w, locked)
+        elif command == "lw":
+            w.moveLockedToWaste()
+        elif command == "lp":
+            print(w.locked)
         elif "wt" in command and len(command) == 3:
             t.addFromWaste(w.waste,command[-1])
-            printTable(t, f, w, locked)
         elif "tf" in command and len(command) == 3:
-            f.addFromTableStack(t.stackAttr(command[-1]))
-            printTable(t, f, w, locked)
+            f.addFromTableStack(t, command[-1])
         elif "tt" in command and len(command) == 4:
-            t.moveInTable(command[-2], command[-1])
-            printTable(t, f, w, locked)
-        elif "tm" in command and len(command) == 5:
-            t.moveInTableMore(command[-3], command[-2], command[-1])
-            printTable(t, f, w, locked)
-        elif "tm" in command and len(command) == 6:
-            t.moveInTableMore(command[-4], command[-3], command[-2] + command[-1])
-            printTable(t, f, w, locked)
-        elif command == "lw":
-            if len(w.waste) == 0 and len(locked) > 0 and len(w.cards.deck) == 0:
-                w.waste.append(locked.pop())
-                printTable(t, f, w, locked)
-            else:
-                print("Waste is not empty or there is no more locked card")
+            t.moveInTableUniversal(command[-2], command[-1])
+        elif "tt" in command and len(command) == 5:
+            t.moveInTableUniversal(command[-3], command[-2], command[-1])
+        elif "tt" in command and len(command) == 6:
+            t.moveInTableUniversal(command[-4], command[-3], command[-2] + command[-1])
         else:
             print("Command unknown")
     
     if f.isWon():
-        print("Congratulations! You've won!")
+        print("****************************************************")
+        print("************Congratulations! You've won!************")
+        print("****************************************************")
